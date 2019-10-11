ce6cc4c15a86bed49e57e4e1e5f3d45854cc32fe
Stepanenko Oksana <lactivo4ka@gmail.com>
2019-10-11 23:58:20 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    let decoded = []; // result array
+    let subexpr = ''; // subexpression with 10 elements from input expression
+    
+    
+    for (let i = 0; i < expr.length; i += 10) {
+        subexpr = expr.slice(i, i + 10); // create subexpression
+        let coded = ''; // coded letter
+        if (subexpr === '**********') 
+            decoded.push(' '); // if input expression includes space
+        else {    // code letter '11' with '-' and '10' with '.'
+            for (let k = 0; k < subexpr.length; k += 2) {
+            if (subexpr[k] === '1' && subexpr[k + 1] === '1') coded += '-';
+                else if (subexpr[k] === '1' && subexpr[k + 1] === '0') coded += '.';
+            }
+        decoded.push(MORSE_TABLE[coded]); // add in result array decoded letter from MORSE_TABLE according to coded letter
+        }
+    }
+    return decoded.join(''); // get string from result array and return
 }
 
 module.exports = {
